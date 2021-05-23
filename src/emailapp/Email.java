2db3f50f943e7a30e3e0487847d2c3dd85c1824e
tunjijnr1 <funbiaboderin@yahoo.com>
2021-05-23 13:02:00 -0400

    Created the Email.java class with guidelines and constructor

---
+++ src/emailapp/Email.java
@@ -0,0 +1,27 @@
+package emailapp;
+
+public class Email {
+   private String firstName;
+   private String lastName;
+   private String password;
+   private String department;
+   private int mailboxCapacity;
+   private String alternateEmail;
+
+   // constructor to receive the first name and last name
+    public Email(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAIL CREATED: "+ this.firstName + " " + this.lastName);
+    }
+
+    // Ask for the departments
+
+    // Generate a random password
+
+    // Set mailbox capacity
+
+    // Set alternate email
+
+    // Change the password
+}
--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -1,4 +1,7 @@
 package emailapp;
 
 public class EmailApp {
+    public static void main(String[] args) {
+        Email emailObj = new Email("Oluwafunbi", "Aboderin");
+    }
 }
