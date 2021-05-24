270ce0de1a81f07a3a7cc852e031aee5e98c3261
tunjijnr1 <funbiaboderin@yahoo.com>
2021-05-24 15:51:29 -0400

    Created the setDepartment class

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
    private String firstName;
    private String lastName;
@@ -13,9 +15,27 @@ public class Email {
         this.firstName = firstName;
         this.lastName = lastName;
         System.out.println("EMAIL CREATED: "+ this.firstName + " " + this.lastName);
+
+        //Call a method asking for the department - return the department
+        this.department = setDepartment();
     }
 
-    // Ask for the departments
+//     Ask for the departments
+    private String setDepartment(){
+        System.out.print("DEPARTMENT CODES\n1 for sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
+        Scanner in = new Scanner(System.in);
+        int departmentChoice = in.nextInt();
+        if (departmentChoice == 1) {
+            return "sales";
+        } else if(departmentChoice == 2){
+            return "Development";
+        } else if( departmentChoice == 3){
+            return "Accounting";
+        } else {
+            return "none";
+        }
+    }
+
 
     // Generate a random password
 
